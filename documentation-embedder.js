4e660b410fa1c897a8ba6eab2e3455f076b49c54
Leon <leon.yee000@gmail.com>
2023-07-30 07:34:09 -0700

    changed to anthropic for summarizing

--- documentation-embedder.js
+++ documentation-embedder.js
@@ -1,9 +1,9 @@
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import { UnstructuredLoader } from "langchain/document_loaders/fs/unstructured";
 import fs from 'fs';
 import * as path from "node:path";
 import { download } from '@guoyunhe/downloader';
 import dotenv from 'dotenv'
+import Anthropic from '@anthropic-ai/sdk'
 
 dotenv.config()
 const lancedb = await import("vectordb");
@@ -14,6 +14,10 @@ const pipe = await pipeline('feature-extraction', 'Xenova/all-MiniLM-L6-v2');
 
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
+const anthropic = new Anthropic({
+    apiKey: process.env.ANTHROPIC_API_KEY,
+});
+
 async function read_data(){
     const unstructuredKey = process.env.UNSTRUCTURED_API_KEY
     if (unstructuredKey == null || unstructuredKey == undefined) {
@@ -23,9 +27,6 @@ async function read_data(){
     }
     var docs = [];
     const docsPath = "data/python-3.11.4-docs-text"
-    const options = {
-        apiKey: unstructuredKey,
-    };
 
     var subfolders = fs.readdirSync(docsPath);
     for (let i = 0; i < subfolders.length; i++) {
@@ -36,24 +37,22 @@ async function read_data(){
             for (const p of fs.readdirSync(subfolder).filter((f) => f.endsWith('.txt'))) {
                 const docPath = path.join(subfolder, p);
                 console.log(docPath);
-                var rawDocument;
-                while (true) {
-                    try {
-                        const loader = new UnstructuredLoader(docPath, options);
-                        rawDocument = await loader.load();
-                        break;
-                    } catch (e) {
-                        console.log('Error loading document:', e);
-                        console.log('Waiting 50 seconds to retry');
-                        await delay(50000);
-                    }
-                }
+
+                var rawDocument = [];
+                var output = fs.readFileSync(docPath).toString();
+                const prompt = `\n\n${Anthropic.HUMAN_PROMPT}: Your task is to read the following code documentation about a python function and summarize it in a few paragraphs. You should include what the function does and a few examples on how to use it, but leave out any unrelated information that is not about the functionality of it in python. Do not begin or end your response with any notes. Here is the documentation file:\n${output}\n\n${Anthropic.AI_PROMPT}:`
+                const completion = await anthropic.completions.create({
+                    model: 'claude-2',
+                    max_tokens_to_sample: 1500,
+                    prompt: prompt,
+                });
+                rawDocument.push({"text": completion.completion, "metadata": {}});
+
                 const metadata = {
                     title: subfolders[i],
                     version: '3.11.4',
                 };
                 rawDocument[0].metadata = Object.assign(rawDocument[0].metadata, metadata);
-                rawDocument[0].metadata['source'] = JSON.stringify(rawDocument[0].metadata['source']);
                 docs = docs.concat(rawDocument);
             }
 
@@ -73,7 +72,6 @@ embed_fun.embed = async function (batch) {
     }
     return (result)
 };
-
 
 
 (async () => {
