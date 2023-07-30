5078323549bef8e310480b3e441373a87822d28c
TevinWang <tevin.wang@warriorlife.net>
2023-07-30 09:48:25 -0700

    merge

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
+import claude
+import pr
+
+# create flask server for nodejs to call
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Hello, World!'
 
-if __name__ == '__main__':
+@app.route("/setrepo/<repo_url>", methods=["GET"])
+def run_stuff(repo_url):
+    repo_url = repo_url.replace("%", "/")
+    # receiving "username/repo" from nodejs
+    claude.main(repo_url)
+    pr.main(repo_url)
+    return repo_url
+
+if __name__ == "__main__":
     app.run(debug=True)
--- drcode/src/index.js
+++ drcode/src/index.js
@@ -11,6 +11,16 @@ root.render(
   </React.StrictMode>
 );
 
+export const initialState = {
+  isLoggedIn: JSON.parse(localStorage.getItem("isLoggedIn")) || false,
+  user: JSON.parse(localStorage.getItem("user")) || null,
+  client_id: process.env.REACT_APP_CLIENT_ID,
+  redirect_uri: process.env.REACT_APP_REDIRECT_URI,
+  client_secret: process.env.REACT_APP_CLIENT_SECRET,
+  proxy_url: process.env.REACT_APP_PROXY_URL
+};
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
